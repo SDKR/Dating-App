43223f10877da089b05d61cd28be47253fb7ed78
SDKR <ecgk@live.dk>
2018-05-17 13:26:53 +0200

    Model, ViewModel, User

---
+++ Dating_App/Model/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dating_App.Model
+{
+    class User
+    {
+
+        public string _name;
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+
+        public User()
+        {
+            this._name = "John 18cm";
+        }
+
+    }
+
+}
+
