2d1a09c972a547f13b118d5670dee97537650436
JannikSvane <J_svane@hotmail.com>
2018-05-22 11:04:02 +0200

    update

--- Dating_App/View/HomePage.xaml.cs
+++ Dating_App/View/HomePage.xaml.cs
@@ -25,14 +25,16 @@ namespace Dating_App.View
             InitializeComponent();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            //(Application.Current.MainWindow.FindName("Frame") as Frame).Content = new Dating_App.View.ProfilPage();
-        }
+       
 
         private void LogAf_Button_Click(object sender, RoutedEventArgs e)
         {
             (Application.Current.MainWindow.FindName("Frame") as Frame).Content = null;
+        }
+
+        private void Profil_HomePage_Button_Click(object sender, RoutedEventArgs e)
+        {
+            (Application.Current.MainWindow.FindName("Frame") as Frame).Content = new Dating_App.View.ProfilPage();
         }
     }
 }
