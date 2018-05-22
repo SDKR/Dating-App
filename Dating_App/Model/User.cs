47ecfeb7a3ab2d307d2c44c7048dc2fbbca60799
SDKR <ecgk@live.dk>
2018-05-22 11:03:47 +0200

    dill

--- Dating_App/Model/User.cs
+++ Dating_App/Model/User.cs
@@ -53,6 +53,55 @@ namespace Dating_App.Model
             set { _bit = value; }
         }
 
+        private int _infoID;
+
+        public int InfoID
+        {
+            get { return _infoID; }
+            set { _infoID = value; }
+        }
+
+        private string _First_name;
+
+        public string First_name
+        {
+            get { return _First_name; }
+            set { _First_name = value; }
+        }
+
+        private string _Last_name;
+
+        public string Last_name
+        {
+            get { return _Last_name; }
+            set { _Last_name = value; }
+        }
+
+        private DateTime _Date;
+
+        public DateTime Date
+        {
+            get { return _Date; }
+            set { _Date = value; }
+        }
+
+        private string _gender;
+
+        public string Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+
 
         /*
          * Methods n' stuff
