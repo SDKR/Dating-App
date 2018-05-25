b269c2a69516aa9069f2bd944bbc7fb38d545c60
JannikSvane <J_svane@hotmail.com>
2018-05-25 12:32:58 +0200

    update

--- Dating_App/View/BeskederPage.xaml.cs
+++ Dating_App/View/BeskederPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Dating_App.Model;
 
 namespace Dating_App.View
 {
@@ -20,9 +21,24 @@ namespace Dating_App.View
     /// </summary>
     public partial class BeskederPage : Page
     {
+        Messages message = new Messages();
+        User user = new User();
+
         public BeskederPage()
         {
             InitializeComponent();
+            //Chat_person_Combobox.ItemsSource = message.CurrentConversationList(Dating_App.Model.User.CurrentUser);
+            List<User> user = message.CurrentConversationList(Dating_App.Model.User.CurrentUser);
+
+            for (int i = 0; i < user.Count; i++)
+            {
+
+                if (user[i].Profile_name != Dating_App.Model.User.CurrentUser.Profile_name)
+                {
+                    Chat_person_Combobox.Items.Add(user[i].Profile_name);
+                }
+                
+            }
         }
 
         private void Hjem_BeskederPage_Button_Click(object sender, RoutedEventArgs e)
@@ -48,6 +64,27 @@ namespace Dating_App.View
         private void LogAf_BeskederPage_Button_Click(object sender, RoutedEventArgs e)
         {
             (Application.Current.MainWindow.FindName("Frame") as Frame).Content = null;
+        }
+
+        private void Chat_person_Combobox_DropDownClosed(object sender, EventArgs e)
+        {
+            Indbakke_datagrid.ItemsSource = message.getConversation(Dating_App.Model.User.CurrentUser.Profile_name, Chat_person_Combobox.Text);
+            NyBesked_Textbox.Text = Chat_person_Combobox.Text;
+        }
+
+        private void Hjem_BeskederPage_Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            (Application.Current.MainWindow.FindName("Frame") as Frame).Content = new Dating_App.View.HomePage();
+        }
+
+        private void Send_BeskedPage_Button_Click(object sender, RoutedEventArgs e)
+        {
+            message.Reciver = NyBesked_Textbox.Text;
+            message.Message = Besked_Textox.Text;
+            message.Sender = Dating_App.Model.User.CurrentUser.Profile_name;
+            message.saveMessage(message);
+            Besked_Textox.Text = null;
+            Indbakke_datagrid.ItemsSource = message.getConversation(Dating_App.Model.User.CurrentUser.Profile_name, Chat_person_Combobox.Text);
         }
     }
 }
