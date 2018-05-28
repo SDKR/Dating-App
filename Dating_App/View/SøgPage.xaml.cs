c371ae0ffe375fae35b8e453821620976a86b610
JannikSvane <J_svane@hotmail.com>
2018-05-28 14:36:47 +0200

    update

--- Dating_App/View/SøgPage.xaml.cs
+++ Dating_App/View/SøgPage.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Dating_App.Model;
+using System.Data;
+using System.IO;
 
 namespace Dating_App.View
 {
@@ -22,6 +24,10 @@ namespace Dating_App.View
     public partial class SøgPage : Page
     {
         MatchingSeeking ms = new MatchingSeeking();
+        User selecteduser = new User();
+        Images imageObj = new Images();
+        ProfileViewPage PVP = new ProfileViewPage();
+
         public SøgPage()
         {
             InitializeComponent();
@@ -122,6 +128,63 @@ namespace Dating_App.View
 
             ResultaterForSøgning_datagrid.ItemsSource = ms.search(ms);
 
+        }
+
+        private void ResultaterForSøgning_datagrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selecteduser = (User)ResultaterForSøgning_datagrid.SelectedItem;
+        }
+
+        private void ResultaterForSøgning_datagrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            
+            PVP.Username_ProfilPage_Label.Content = selecteduser.FK_profile_name;
+            PVP.FornavnData_ProfilPage_label.Content = selecteduser.First_name;
+            PVP.EfternavnData_ProfilPage_label.Content = selecteduser.Last_name;
+            PVP.FødselsdagData_ProfilPage_label.Content = selecteduser.Date;
+            PVP.KønData_ProfilPage_label.Content = selecteduser.Gender;
+            PVP.StatusData_ProfilPage_label.Content = selecteduser.Status;
+            PVP.SøgerData_ProfilPage_label.Content = selecteduser.Seeking;
+            PVP.InteresseretIData_ProfilPage_label.Content = selecteduser.SexualOrientation;
+            PVP.HøjdeData_ProfilPage_label.Content = selecteduser.Height;
+            PVP.VægtData_ProfilPage_label.Content = selecteduser.Weight;
+            PVP.ØjenfarveData_ProfilPage_label.Content = selecteduser.Eyecolor;
+            PVP.HårfarveData_ProfilPage_label.Content = selecteduser.Haircolor;
+            PVP.BørnData_ProfilPage_label.Content = selecteduser.Children;
+            PVP.KropsTypeData_ProfilPage_label.Content = selecteduser.Body_Type;
+            PVP.PostNummerData_ProfilPage_label.Content = selecteduser.Postcode;
+            PVP.BeskrivDigSelv_ProfilPage_TextBox.Text = selecteduser.About_yourself;
+            LoadPicture();
+
+            (Application.Current.MainWindow.FindName("Frame") as Frame).Content = PVP;
+        }
+
+        public void LoadPicture()
+        {
+            DataSet ds = imageObj.getImage(selecteduser.Profile_name);
+            DataTable dataTable = ds.Tables[0];
+            foreach (DataRow row in dataTable.Rows)
+            {
+                if (row[0].ToString() != null)
+                {
+                    //Store binary data read from the database in a byte array
+                    byte[] blob = (byte[])row[2];
+                    MemoryStream stream = new MemoryStream();
+                    stream.Write(blob, 0, blob.Length);
+                    stream.Position = 0;
+
+                    System.Drawing.Image img = System.Drawing.Image.FromStream(stream);
+                    BitmapImage bi = new BitmapImage();
+                    bi.BeginInit();
+
+                    MemoryStream ms = new MemoryStream();
+                    img.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                    ms.Seek(0, SeekOrigin.Begin);
+                    bi.StreamSource = ms;
+                    bi.EndInit();
+                    PVP.ProfilBillede_ProfilPage_Image.Source = bi;
+                }
+            }
         }
     }
 }
