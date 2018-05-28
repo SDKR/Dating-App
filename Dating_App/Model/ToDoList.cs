b74dc4b35d5a8cb70d6102c1c8de11ed0c53c5df
JannikSvane <J_svane@hotmail.com>
2018-05-28 11:03:30 +0200

    update

---
+++ Dating_App/Model/ToDoList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dating_App.Model
+{
+    class ToDoList
+    {
+
+
+        // Lav en lækker Chat(True/false)
+        // Lav en ny Page til "View Profil"
+        // Tjek for Crash
+        // "Like Buttom"
+        // Orden og columns i datagrids
+
+
+
+
+
+
+
+
+
+    }
+}
